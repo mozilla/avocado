43dbb235fb21cf590d373233c94ad66695ef3b46
Jenny <50588415+zyjenny@users.noreply.github.com>
2019-05-27 17:17:59 -0400

    Added prettier, fixes #11 (#17)
    * added prettier, ran on /src, fixes #11
    
    * added lint-fix script to package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   },
   'extends': [
     'eslint:recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'plugin:prettier/recommended'
   ],
   'globals': {
     'Atomics': 'readonly',
