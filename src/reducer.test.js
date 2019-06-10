673d4dad194a45e06577678766986092bc3ccf47
jenny zhang <jezhang@mozilla.com>
2019-06-10 15:05:14 -0400

    reducer tests pass

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -7,4 +7,10 @@ describe("Reducer", () => {
     let expectedState = fromJS({"items": []});
     expect(experiments(undefined, {})).toEqual(expectedState);
   });
+
+  it("should test EXPERIMENT_DATA_RECEIVED case", () => { 
+    let expectedState = fromJS({"items": "[1,2,3]"});
+    let reducerResult = experiments(undefined, { type: "EXPERIMENT_DATA_RECEIVED", data: "[1,2,3]" })
+    expect(reducerResult).toEqual(expectedState);
+  });
 });
