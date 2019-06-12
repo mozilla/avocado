23957d6814d4db45c8645bdf96d8d466436c120a
jenny zhang <jezhang@mozilla.com>
2019-06-12 14:39:22 -0400

    renamed App.jsx to ExperimentList.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import App from "./components/App";
+import ExperimentList from "./components/ExperimentList";
 import { createStore, applyMiddleware } from "redux";
 import { combineReducers } from "redux-immutable";
 import experiments from "./state/reducer";
@@ -27,7 +27,7 @@ const store = createStore(
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ExperimentList />
   </Provider>,
   document.getElementById("root")
 );
