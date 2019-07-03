ebda000237993cd43fa8e6b905b9f2c452fd3444
jenny zhang <jezhang@mozilla.com>
2019-07-03 15:41:31 -0400

    all test pass

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import ExperimentList from "./components/experiments/ExperimentList";
-import DatePicker from "./components/dates/DatePicker";
+import ConnectedDatePicker from "./components/dates/DatePicker";
 import { createStore, applyMiddleware } from "redux";
 import { combineReducers } from "redux-immutable";
 import experiments from "./state/experiments/reducer";
