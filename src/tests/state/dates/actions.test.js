e7f21a46d4f5b90060570e56df9cf5efd8cf0b14
jenny zhang <jezhang@mozilla.com>
2019-07-03 15:41:31 -0400

    all test pass

--- src/tests/state/dates/actions.test.js
+++ src/tests/state/dates/actions.test.js
@@ -1,13 +1,9 @@
-import { setStartDate } from "../../../state/dates/actions";
-import { START_DATE_SELECTED } from "../../../state/action-types";
+import { setDate } from "../../../state/dates/actions";
+import { START_DATE_SELECTED, END_DATE_SELECTED } from "../../../state/action-types";
 
 describe("testing actions", () => {
-  beforeEach(() => {
-    fetch.resetMocks();
-  });
-
   it("should set the start date", done => {
-    let setDate = setStartDate("2019-06-19");
+    let setDateAction = setDate("2019-06-19", START_DATE_SELECTED);
 
     let dispatch = action => {
         expect(action.data).toEqual("2019-06-19");
@@ -15,6 +11,18 @@ describe("testing actions", () => {
         done();
       };
 
-    setDate(dispatch);
+    setDateAction(dispatch);
+  });
+
+  it("should set the end date", done => {
+    let setDateAction = setDate("2019-06-20", END_DATE_SELECTED);
+
+    let dispatch = action => {
+        expect(action.data).toEqual("2019-06-20");
+        expect(action.type).toEqual(END_DATE_SELECTED);
+        done();
+      };
+
+    setDateAction(dispatch);
   });
 });
--- src/tests/state/dates/reducer.test.js
+++ src/tests/state/dates/reducer.test.js
@@ -1,6 +1,6 @@
 import date from "../../../state/dates/reducer";
 import { fromJS } from "immutable";
-import { START_DATE_SELECTED } from "../../../state/action-types";
+import { START_DATE_SELECTED, END_DATE_SELECTED } from "../../../state/action-types";
 
 describe("Date Reducer", () => {
   it("should test default action for start date selection", () => {
@@ -13,6 +13,15 @@ describe("Date Reducer", () => {
     let reducerResult = date(undefined, {
       type: START_DATE_SELECTED,
       data: "2018-07-22"
+    });
+    expect(reducerResult).toEqual(expectedState);
+  });
+  
+  it("should test end date selected case", () => {
+    let expectedState = fromJS({"dates": "2018-07-21"});
+    let reducerResult = date(undefined, {
+      type: END_DATE_SELECTED,
+      data: "2018-07-21"
     });
     expect(reducerResult).toEqual(expectedState);
   });
