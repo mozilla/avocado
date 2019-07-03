731ee1a2c6466cced12bf79e33dc4a03ee121efd
jenny zhang <jezhang@mozilla.com>
2019-07-03 15:46:05 -0400

    linter cleanup

--- src/components/dates/DatePicker.jsx
+++ src/components/dates/DatePicker.jsx
@@ -1,30 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
-import { getStartDate } from "../../state/dates/selectors";
 import { setDate } from "../../state/dates/actions";
-
 
 import PropTypes from "prop-types";
 
 export class DatePicker extends React.Component {
-
   constructor(props) {
     super(props);
     this.onChange = this.onChange.bind(this);
   }
 
   onChange(event) {
-    this.props.dispatch(setDate(event.target.value, this.props.onChangeAction))
-  };
+    this.props.dispatch(setDate(event.target.value, this.props.onChangeAction));
+  }
 
   render() {
     return (
       <div>
-        <input
-          type="date"
-          onChange={this.onChange}
-          value={this.props.value}
-        />
+        <input type="date" onChange={this.onChange} value={this.props.value} />
       </div>
     );
   }
@@ -32,13 +25,13 @@ export class DatePicker extends React.Component {
 
 DatePicker.propTypes = {
   value: PropTypes.string,
-  setDate: PropTypes.func
+  setDate: PropTypes.func,
+  dispatch: PropTypes.func,
+  onChangeAction: PropTypes.string
 };
 
 const mapStateToProps = state => ({
   value: state.value
 });
 
-export default connect(
-  mapStateToProps
-)(DatePicker);
+export default connect(mapStateToProps)(DatePicker);
