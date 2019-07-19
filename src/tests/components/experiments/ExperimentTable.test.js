3e21b24a20968c5f5d1723604c353c6e9c74c087
jenny zhang <jezhang@mozilla.com>
2019-07-19 11:17:29 -0400

    condensed ExperimentTable tests

--- src/tests/components/experiments/ExperimentTable.test.js
+++ src/tests/components/experiments/ExperimentTable.test.js
@@ -11,16 +11,12 @@ describe("ExperimentTable component", () => {
           filteredExperiments={fromJS([{name: "Name", start_date: "2018-08-22", end_date: "2018-08-23", status: "live"}])}
         />
       );
-    const thead = container.find('thead');
-    const headerRow = thead.find('tr');
-    const tbody = container.find('tbody');
-    const rows = tbody.find('tr');
-
-    it("should render an experiment table with one thead tag", () => {
-        expect(thead).toHaveLength(1);
-    });
     
-    it("should render an experiment table with correct header rows defining the head of the columns of the table", () => {
+    it("should render an experiment table with a correct header row defining the head of the columns of the table", () => {
+        const thead = container.find('thead');
+        expect(thead).toHaveLength(1);
+
+        const headerRow = thead.find('tr');
         headerRow.forEach((tr) => {
             const cells = tr.find('th');
             expect(cells).toHaveLength(4);
@@ -31,15 +27,12 @@ describe("ExperimentTable component", () => {
         });
     });
 
-    it("should render an experiment table with one tbody tag", () => {
+    it("should render experiment table rows inside tbody tag, with column info of: name, start_date, end_date, status", () => {
+        const tbody = container.find('tbody');
         expect(tbody).toHaveLength(1);
-    });
 
-    it("should render an experiment table with one row", () => {
+        const rows = tbody.find('tr');
         expect(rows).toHaveLength(1);
-    });
-
-    it("should render experiment table rows with column info of: name, start_date, end_date, status", () => {
         rows.forEach((tr) => {
             const cells = tr.find('td');
             expect(cells).toHaveLength(4);
