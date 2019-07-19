86d404b51d080669eba5d51b2f2b33faf9a93086
jenny zhang <jezhang@mozilla.com>
2019-07-18 20:03:12 -0400

    ran linting again

--- src/components/experiments/ExperimentTable.jsx
+++ src/components/experiments/ExperimentTable.jsx
@@ -5,28 +5,28 @@ import { List } from "immutable";
 export class ExperimentTable extends React.Component {
   render() {
     return (
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Start Date</th>
-              <th>End Date</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.props.filteredExperiments.map(function(item, key) {
-              return (
-                <tr key={key}>
-                  <td>{item.get("name")}</td>
-                  <td>{new Date(item.get("start_date")).toGMTString()}</td>
-                  <td>{new Date(item.get("end_date")).toGMTString()}</td>
-                  <td>{item.get("status")}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Start Date</th>
+            <th>End Date</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.filteredExperiments.map(function(item, key) {
+            return (
+              <tr key={key}>
+                <td>{item.get("name")}</td>
+                <td>{new Date(item.get("start_date")).toGMTString()}</td>
+                <td>{new Date(item.get("end_date")).toGMTString()}</td>
+                <td>{item.get("status")}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     );
   }
 }
