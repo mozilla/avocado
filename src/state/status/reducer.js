e178981bf47bb0b32a4e5a6e0eb10d2ab74268ea
jenny zhang <jezhang@mozilla.com>
2019-07-30 14:07:43 -0400

    minor change to reducer

--- src/state/status/reducer.js
+++ src/state/status/reducer.js
@@ -4,7 +4,7 @@ import { STATUS_SELECTED } from "../action-types";
 export const selectedStatus = (state = null, action) => {
     switch (action.type) {
       case STATUS_SELECTED:
-        return action.data !== "" ? action.data : null;
+        return action.data ? action.data : null;
       default:
         return state;
     }
