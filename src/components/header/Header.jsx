12ee06aac5308d0365568f7763b5fff76aee238c
jenny zhang <jezhang@mozilla.com>
2019-08-20 16:52:06 -0400

    removed sb-admin

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import '../../assets/header.css';
-import '../../assets/css/sb-admin-2.css';
 
 export class Header extends React.Component {
   render() {
