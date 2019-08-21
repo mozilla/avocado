9b03674c7038b97a1eb874e02235d0ab9fd1531b
jenny zhang <jezhang@mozilla.com>
2019-08-21 13:03:35 -0400

    graph labels fixed

--- src/components/experiments/ExperimentGraph.jsx
+++ src/components/experiments/ExperimentGraph.jsx
@@ -12,33 +12,27 @@ export class ExperimentGraph extends React.Component {
     let data = [
       {
         type: 'scatter',  // all "scatter" attributes: https://plot.ly/javascript/reference/#scatter
-        x: [1, 2, 3, 4, 5, 6, 7],     // more about "x": #scatter-x
-        y: this.props.median,     // #scatter-y
-        marker: {         // marker is an object, valid marker keys: #scatter-marker
-          color: 'rgb(16, 32, 77)' // more about "marker.color": #scatter-marker-color
+        x: ["Draft", "Review", "Ship", "Accepted", "Live", "Complete"],
+        y: this.props.median,
+        marker: {
+          color: 'rgb(16, 32, 77)'
         }
       },
       {
         type: 'bar',      // all "bar" chart attributes: #bar
-        x: [1, 2, 3, 4, 5, 6, 7],     // more about "x": #bar-x
-        y: this.props.median,     // #bar-y
-        name: 'bar chart example' // #bar-name
+        x: ["Draft", "Review", "Ship", "Accepted", "Live", "Complete"],
+        y: this.props.median,
+        name: 'bar graph'
       }
     ];
-    let layout = {                     // all "layout" attributes: #layout
-      title: 'median',  // more about "layout.title": #layout-title
-      xaxis: {                  // all "layout.xaxis" attributes: #layout-xaxis
-        title: 'time'         // more about "layout.xaxis.title": #layout-xaxis-title
+    let layout = {
+      title: '',
+      xaxis: {
+        title: 'stage'
       },
-      annotations: [            // all "annotation" attributes: #layout-annotations
-        {
-          text: 'simple annotation',    // #layout-annotations-text
-          x: 0,                         // #layout-annotations-x
-          xref: 'paper',                // #layout-annotations-xref
-          y: 0,                         // #layout-annotations-y
-          yref: 'paper'                 // #layout-annotations-yref
-        }
-      ]
+      yaxis: {
+        title: 'median number of days spent in stage',
+      }
     };
     let config = {
       showLink: false,
