07a7559e0b6c304c36e17b873691ae9284b0bf50
jenny zhang <jezhang@mozilla.com>
2019-08-23 10:51:45 -0400

    added graph and some tests

--- src/components/experiments/ExperimentGraph.jsx
+++ src/components/experiments/ExperimentGraph.jsx
@@ -2,43 +2,38 @@ import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getMedianArray } from "../../state/experiments/selectors";
-import createPlotlyComponent from 'react-plotlyjs';
-import Plotly from 'plotly.js/dist/plotly-cartesian';
-const PlotlyComponent = createPlotlyComponent(Plotly);
+import createPlotlyComponent from "react-plotlyjs";
+import Plotly from "plotly.js/dist/plotly-cartesian";
+import { List } from "immutable";
 
 export class ExperimentGraph extends React.Component {
   render() {
-    console.log(this.props.median)
+    const PlotlyComponent = createPlotlyComponent(Plotly);
     let data = [
       {
-        type: 'scatter',  // all "scatter" attributes: https://plot.ly/javascript/reference/#scatter
-        x: [1, 2, 3, 4, 5, 6, 7],     // more about "x": #scatter-x
-        y: this.props.median,     // #scatter-y
-        marker: {         // marker is an object, valid marker keys: #scatter-marker
-          color: 'rgb(16, 32, 77)' // more about "marker.color": #scatter-marker-color
-        }
+        type: "scatter", // all "scatter" attributes: https://plot.ly/javascript/reference/#scatter
+        x: ["Draft", "Review", "Ship", "Accepted", "Live", "Complete"],
+        y: this.props.median,
+        marker: {
+          color: "rgb(16, 32, 77)"
+        },
+        name: "line graph"
       },
       {
-        type: 'bar',      // all "bar" chart attributes: #bar
-        x: [1, 2, 3, 4, 5, 6, 7],     // more about "x": #bar-x
-        y: this.props.median,     // #bar-y
-        name: 'bar chart example' // #bar-name
+        type: "bar", // all "bar" chart attributes: #bar
+        x: ["Draft", "Review", "Ship", "Accepted", "Live", "Complete"],
+        y: this.props.median,
+        name: "bar graph"
       }
     ];
-    let layout = {                     // all "layout" attributes: #layout
-      title: 'median',  // more about "layout.title": #layout-title
-      xaxis: {                  // all "layout.xaxis" attributes: #layout-xaxis
-        title: 'time'         // more about "layout.xaxis.title": #layout-xaxis-title
+    let layout = {
+      title: "",
+      xaxis: {
+        title: "Phase"
       },
-      annotations: [            // all "annotation" attributes: #layout-annotations
-        {
-          text: 'simple annotation',    // #layout-annotations-text
-          x: 0,                         // #layout-annotations-x
-          xref: 'paper',                // #layout-annotations-xref
-          y: 0,                         // #layout-annotations-y
-          yref: 'paper'                 // #layout-annotations-yref
-        }
-      ]
+      yaxis: {
+        title: "Median number of days spent in phase"
+      }
     };
     let config = {
       showLink: false,
@@ -47,14 +42,14 @@ export class ExperimentGraph extends React.Component {
 
     return (
       <div>
-        <PlotlyComponent className="whatever" data={data} layout={layout} config={config}/>
+        <PlotlyComponent data={data} layout={layout} config={config} />
       </div>
     );
   }
 }
 
 ExperimentGraph.propTypes = {
-  median: PropTypes.instanceOf(Array)
+  median: PropTypes.instanceOf(List)
 };
 
 const mapStateToProps = state => ({
